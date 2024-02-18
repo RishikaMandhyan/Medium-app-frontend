6ff0129a0e2b5743fc97da0c94df493d8a0601b9
RishikaMandhyan <rishikamandhyan@gmail.com>
2024-02-18 16:47:32 +0530

    redirect fixes

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-const ProtectedRoutes = ({currentuser}) => {
-  return currentuser ? <Outlet /> : <Navigate to="/login" />;
+const ProtectedRoutes = ({ currentuser }) => {
+  return currentuser ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoutes;
